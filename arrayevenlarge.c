68e26736e23159e8df8bbd3f5a8865cb96a7f864
Anurag Singh <55041899+anurag03-10@users.noreply.github.com>
2020-10-01 09:08:20 +0530

    Update arrayevenlarge.c
    The last programme was insufficient to find the even largest number.

--- arrayevenlarge.c
+++ arrayevenlarge.c
@@ -1,13 +1,29 @@
 #include<stdio.h>
 void main()
 {
- int g,a=0, b[10],s=0,p=-32765;
+ int g,a=0, b[10],s=0,p,count=0;
  printf("Enter %d numbers  ",g);
  
  for(;a<g;a++)
  {
   scanf("%d",&b[a]);
+  if((a==0)&&(b[a]%2==0))
+  {
+   p=b[0];
+   count++;
+  }
+  else
+  {
+   if(b[a]%2==0)
+    p=b[a];
+  } 
  }
-  printf("\n largest no. among all elements in the array is %d ",p);
- 
+  if(count==0)
+  {
+   printf("There is no even number in the array \n");
+  }
+  else
+  {
+   printf("\n largest no. among all elements in the array is %d ",p);
+  } 
 }
